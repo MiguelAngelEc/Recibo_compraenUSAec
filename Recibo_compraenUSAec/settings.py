bddfe222db0afada654f757f962e631fba52f402
MigueEC <filia.castillo@gmail.com>
2024-10-12 17:12:15 -0500

    Ok

--- Recibo_compraenUSAec/settings.py
+++ Recibo_compraenUSAec/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!=j^+cpl5!yab(!+9$cxjh(#y-a!8@ronmw!!2&jpvmyaurrqm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
