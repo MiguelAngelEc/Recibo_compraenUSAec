d7917cd33057163ec6974ee395ab8a6a51e4a291
MigueEC <filia.castillo@gmail.com>
2024-10-22 11:59:34 -0500

    Keys Server

--- Recibo_compraenUSAec/settings.py
+++ Recibo_compraenUSAec/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', '58ecb394d68d734caa2590b980173e3a')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'f9e5201fb476f731aa96bf944b982164')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
